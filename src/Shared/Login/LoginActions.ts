a669a0f15cc5abf7cde0c6feb57fbbbd853464ab
Slavik <slavikyarkin@gmail.com>
2021-05-22 14:06:27 +0300

    add files for login

--- src/Shared/Login/LoginActions.ts
+++ src/Shared/Login/LoginActions.ts
@@ -4,4 +4,4 @@ import { LoginModel } from "./LoginModel";
 export const logIn = createAction('login/LOG_IN')<LoginModel>();
 
 export const logInSuccess = createAction('login/LOG_IN_SUCCESS')<LoginModel>();
-export const logInFail = createAction('login/LOG_IN_FAIL')<LoginModel>();
+export const logInFail = createAction('login/LOG_IN_FAIL')<Error>();
