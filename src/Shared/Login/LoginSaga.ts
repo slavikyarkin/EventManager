21fd56daa6b729793d25c9e55eaae34ead22e08e
Slavik <slavikyarkin@gmail.com>
2021-05-27 19:09:47 +0300

    fix

--- src/Shared/Login/LoginSaga.ts
+++ src/Shared/Login/LoginSaga.ts
@@ -2,8 +2,8 @@ import { call, put, take, takeEvery, takeLatest } from 'redux-saga/effects'
 import { action, ActionType, getType } from 'typesafe-actions'
 import * as Api from "./LoginAPI";
 import * as actions from "./LoginActions";
-import { LoginModel } from './LoginModel';
-import { LoginData } from './LoginData';
+import { LoginModel, TokenModel } from './LoginModel';
+import { LoginData, TokenData } from './LoginData';
 
 export function* loginSaga() {
     yield takeLatest(getType(actions.logIn), logIn);
@@ -11,10 +11,11 @@ export function* loginSaga() {
 
 function* logIn(action: ActionType<typeof actions.logIn>) {
     try {   
-        const data: LoginData = yield call(Api.logIn, action.payload);
-        // const model: LoginModel = mapper.mapToModel(data);
+        const data: TokenData = yield call(Api.logIn, action.payload);
+        const model: TokenModel = {...data}
 
-        // sessionStorage.setItem('token', model.token);
+        sessionStorage.setItem('token', model.token);
+        
         // yield put(actions.logInSuccess(model));
     } catch (e) {
         yield put(actions.logInFail(e));
