61dd2ff75adbcab52dc06ac2ebc029c2e9a80c2c
slavikyarkin <slavikyarkin@gmail.com>
2021-06-24 07:23:36 +0300

    add snackbar

---
+++ src/Shared/Snackbar/SnackbarContainer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function CustomizedSnackbars() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          This is a success message!
+        </Alert>
+      </Snackbar>
+    </>
+  );
+}
